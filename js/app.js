ee071d407ef9fa6836ce10575654252e15bd698e
ashleynadolphson <ashleynadolphson@gmail.com>
2025-04-17 15:11:26 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
-onst observer = new IntersectionObserver((entries) => {
-
+const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-
     if (entry.isIntersecting) {
-
       entry.target.classList.add("show");
-
-} else {
-
+    } else {
       entry.target.classList.remove("show");
-
-}
-
+    }
   });
-
 });
 
-
 const hiddenElements = document.querySelectorAll(".hidden");
-
 hiddenElements.forEach((el) => observer.observe(el));
